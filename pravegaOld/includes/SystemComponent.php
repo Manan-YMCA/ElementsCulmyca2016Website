b5a909ab24a74f920d61f3dda9f5aeb053bed414
sagnikd96 <sagnikd96@gmail.com>
2015-10-19 01:04:04 +0530

    Get the local php working....Barely

--- pravegaOld/includes/SystemComponent.php
+++ pravegaOld/includes/SystemComponent.php
@@ -6,20 +6,20 @@ class SystemComponent {
 
     function getsettings() {
 
-        $settings['siteDir'] = 'www/wamp/admin_panel';
+        $settings['siteDir'] = ' C:\\wamp\\www\\pravega\\pravegaOld\\';
         $settings['dbhost'] = 'localhost';
 		
-        /*$settings['dbusername'] = 'root';
+        $settings['dbusername'] = 'root';
         $settings['dbpassword'] = '';
         $settings['dbname'] = 'pravega';
 		
-		$settings['dbusername'] = 'mediatoo_pravega';
+		/*$settings['dbusername'] = 'mediatoo_pravega';
         $settings['dbpassword'] = 'pravega';
         $settings['dbname'] = 'mediatoo_pravega';*/
 		
-		$settings['dbusername'] = 'pravega';
+		/*$settings['dbusername'] = 'pravega';
         $settings['dbpassword'] = 'pravega@123';
-        $settings['dbname'] = 'pravega';
+        $settings['dbname'] = 'pravega';*/
 		
         $settings['formaction'] = htmlentities($_SERVER["PHP_SELF"]);
 
