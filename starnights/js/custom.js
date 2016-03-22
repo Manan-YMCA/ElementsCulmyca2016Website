9c544e6c47efa70162879aae43398be85ba9d43d
satwickdash <satwick1995@gmail.com>
2016-03-22 12:49:10 +0530

    Everything working

--- starnights/js/custom.js
+++ starnights/js/custom.js
@@ -1,4 +1,4 @@
-/* ----------------------------- 
+/* -----------------------------
 Pre Loader
 ----------------------------- */
 $(window).load(function() {
@@ -8,7 +8,7 @@ $(window).load(function() {
 });
 
 
-/* ----------------------------- 
+/* -----------------------------
 Backgroung slider
 ----------------------------- */
 $(window).ready(function() {
@@ -23,12 +23,12 @@ $(window).ready(function() {
 });
 
 
-				
 
-/* ----------------------------- 
+
+/* -----------------------------
 Scroll into viewPort Animation
 ----------------------------- */
-$(document).ready(function() {	
+$(document).ready(function() {
 	'use strict';
 	$('.animated').appear(function() {
 		var element = $(this),
@@ -45,12 +45,12 @@ $(document).ready(function() {
 			}
 	});
 });
-	
 
-/* ----------------------------- 
+
+/* -----------------------------
 NiceScroll
------------------------------ */	
-$(document).ready(function() { 
+----------------------------- */
+$(document).ready(function() {
 	'use strict';
     $("html").niceScroll({
 		cursorcolor: '#E74C3C',
@@ -67,22 +67,22 @@ $(document).ready(function() {
 
 
 
-				
-/* ----------------------------- 
+
+/* -----------------------------
 Card Style Script
------------------------------ */					
+----------------------------- */
 $(document).ready(function() {
 	'use strict';
 	var $el 			= $( '#card-ul' ),
 		sectionFeature  = $('#section-feature'),
 		baraja 			= $el.baraja();
-	
+
 		if ( $(window).width() > 480) {
 			sectionFeature.appear(function(){
 				baraja.fan({
 					speed : 1500,
 					easing : 'ease-out',
-					range : 100,
+					range : 75,
 					direction : 'right',
 					origin : { x : 50, y : 200 },
 					center : true
@@ -92,18 +92,18 @@ $(document).ready(function() {
 				baraja.fan({
 					speed : 500,
 					easing : 'ease-out',
-					range : 100,
+					range : 75,
 					direction : 'right',
 					origin : { x : 50, y : 200 },
 					center : true
 				});
-			}); 
+			});
 		} else {
 			sectionFeature.appear(function(){
 				baraja.fan({
 					speed : 1500,
 					easing : 'ease-out',
-					range : 80,
+					range : 60,
 					direction : 'left',
 					origin : { x : 200, y : 50 },
 					center : true
@@ -113,14 +113,14 @@ $(document).ready(function() {
 				baraja.fan({
 					speed : 500,
 					easing : 'ease-out',
-					range : 80,
+					range : 60,
 					direction : 'left',
 					origin : { x : 200, y : 50 },
 					center : true
 				});
 			});
 		}
-		
+
 	// Feature navigation
 	$('#feature-prev').on( 'click', function( event ) {
 		baraja.previous();
@@ -129,14 +129,14 @@ $(document).ready(function() {
 	$('#feature-next').on( 'click', function( event ) {
 		baraja.next();
 	});
-	
+
 	// close Features
 	$('#feature-close').on( 'click', function( event ) {
 		baraja.close();
-	});	
+	});
 });
 
-/* ----------------------------- 
+/* -----------------------------
 Fitvids init
 ----------------------------- */
  $(document).ready(function(){
@@ -145,7 +145,7 @@ Fitvids init
  });
 
 
-/* ----------------------------- 
+/* -----------------------------
 IE 9 Placeholder fix
 ----------------------------- */
 $('[placeholder]').focus(function() {
@@ -164,24 +164,24 @@ $('[placeholder]').focus(function() {
 
 
 
-/* ----------------------------- 
+/* -----------------------------
 Screenshot Load
------------------------------ */	
+----------------------------- */
 $(document).ready(function() {
 	'use strict';
 	$('.view-project').on('click', function(e) {
 		e.preventDefault();
-		
+
 		var href 			= $(this).attr('href') + ' .portfolio-project',
 			portfolioWrap	= $('.porfolio-container'),
 			contentLoaded 	= $('#portfolio-load'),
 			offset			= $('#section-screenshots').offset().top;
-		
+
 		portfolioWrap.animate({'left':'-120%'},{duration:400,queue:false});
 		portfolioWrap.fadeOut(400);
 		$('html, body').animate({scrollTop: offset},{duration:800,queue:true});
 		setTimeout(function(){ $('#portfolio-loader').fadeIn('fast'); },300);
-		
+
 		setTimeout(function(){
             contentLoaded.load(href, function(){
                 $('#portfolio-loader').fadeOut('fast');
@@ -189,33 +189,33 @@ $(document).ready(function() {
                 $('.back-button').fadeIn(600);
             });
         },400);
-		
-		
-		
+
+
+
 	});
-	
+
 	$('.backToProject').on('click', function(e){
 		e.preventDefault();
-		
+
 		var portfolioWrap	= $('.porfolio-container'),
 			contentLoaded 	= $('#portfolio-load');
-			
+
 		contentLoaded.animate({'left':'105%'},{duration:400,queue:false}).delay(300).fadeOut(400);
         $(this).parent().fadeOut(400);
 		setTimeout(function(){
             portfolioWrap.animate({'left':'0'},{duration:400,queue:false});
             portfolioWrap.fadeIn(600);
         },500);
-		
+
 	});
 
 });
 
 
-							
-/* ----------------------------- 
+
+/* -----------------------------
 BxSlider
------------------------------ */		
+----------------------------- */
 $(document).ready(function() {
 	'use strict';
 	$('.testimonial-slider').bxSlider({
@@ -223,11 +223,11 @@ $(document).ready(function() {
 		pager: true,
 		touchEnabled: true,
 		controls: false
-	});	
+	});
 });
 
-				
-/* ----------------------------- 
+
+/* -----------------------------
 Main navigation
 ----------------------------- */
 $(document).ready(function() {
@@ -241,11 +241,11 @@ $(document).ready(function() {
 		var scrollPos = $(window).scrollTop();
 		scrollPos > 220 ? $('.sticky-section').addClass('nav-bg') : $('.sticky-section').removeClass('nav-bg');
 	});
-});				
-				
-				
-/* ----------------------------- 
-MailCimp Plugin Script 
+});
+
+
+/* -----------------------------
+MailCimp Plugin Script
 ----------------------------- */
 $(document).ready(function() {
 	'use strict';
@@ -253,7 +253,7 @@ $(document).ready(function() {
 		callback: mailchimpCallback,
 		url: 'YOUR_URL' /* Replace it with your custom URL inside '' */
 	});
-	
+
 	function mailchimpCallback(resp) {
 		 if(resp.result === 'success') {
 			$('.subscription-success')
@@ -262,24 +262,24 @@ $(document).ready(function() {
 				.fadeIn(1000);
 
 			$('.subscription-success').fadeOut(8000);
-			
+
 		} else if(resp.result === 'error') {
 			$('.subscription-failed')
 				.html(resp.msg)
 				.delay(500)
 				.fadeIn(1000);
-				
+
 			$('.subscription-failed').fadeOut(5000);
 		}
 		$('#subscription-form .input-email').val('');
 	};
 });
 
-			
 
-/* ----------------------------- 
+
+/* -----------------------------
 Contact form
------------------------------ */			
+----------------------------- */
 $(document).ready(function() {
 	'use strict';
 	$('form.contact-form').on('submit', function(e) {
@@ -301,4 +301,3 @@ $(document).ready(function() {
 	});
 
 });
-			
